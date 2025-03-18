586b9743bf62a12adcfdba7fb0a479a741552b42
felipeclarindo <fglpc2@gmail.com>
2025-03-17 22:50:14 -0300

    feat(app): Starting cli development.

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using src.library.Models.;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    protected List<TeamMembers> TeamMembers = new List<>();
+
+    static void Main()
+    {
+        while (true)
+        {   
+            RegisterOwner();
+
+            string option = MenuOptions();
+
+            switch ()
+            {
+                case "1":
+                    RegisterMember();
+                    break;
+                case "2":
+                    ListMembers();
+                    break;
+                case "3":
+                    Console.WriteLine("Exiting...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid option!");
+                    break;
+            }
+        }
+    }
+
+    static string menuOptions() {
+
+            Console.WriteLine("\n--- Team Track ---");
+            Console.WriteLine("1. Add Member");
+            Console.WriteLine("2. List Members");
+            Console.WriteLine("3. View Member");
+            Console.WriteLine("3. Exit");
+            Console.Write("Choise an option: ");
+            
+            string option = Console.ReadLine();
+            return option;
+    }
+
+    static void RegisterOwner() {
+        
+    }
+
+    static void RegisterMember()
+    {
+        
+        Console.WriteLine("To start the system its need the owner's information.");
+        Console.Write("\nOwner name: ")
+        string name = Console.ReadLine();
+        Console.Write("Owner department: ");
+        string department = Console.ReadLine();
+        Owner owner = new Owner(name, department);
+    }
+
+    static void ViewMember() {
+
+    }
+
+    static void ListMembers()
+    {
+        foreach (var member in teamMembers )
+        {
+            member.DisplayInfo();
+        }
+    }
+}
